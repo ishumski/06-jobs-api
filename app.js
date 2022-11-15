d6319ebba9fb6fb8b443b9eedf3eb9298b70251e
Ilya Shumski <ilya.shumski@exposit.com>
2022-11-15 11:08:38 +0300

    add routes | controllers | user model

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//connect DB
+
+//routers
+const authRouter = require('./routes/auth')
+const jobRouter = require('./routes/jobs')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
-
+const connectDB = require('./db/connect')
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobRouter)
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+const register = async (req, res)=>{
+    res.send('register')
+}
+const login = async (req, res)=>{
+    res.send('login')
+}
+
+module.exports = { register, login }
